22e30b8ae7be3160358d419ea378fbd117288d68
valentin lechner <valentin_lechner@dismail.de>
2023-12-06 19:11:33 +0100

    bare implementation done

--- ddns.ts
+++ ddns.ts
@@ -1,12 +1,28 @@
 #!/usr/bin/env -S deno run --allow-net --allow-env --allow-read
 
 import njalla from "npm:njalla-dns@2.0.3";
+
 const domain = Deno.env.get("DDNS_DOMAIN");
 const dns = njalla(Deno.env.get("DDNS_API_KEY"));
-const domains = await dns.getDomains();
+const records = await dns.getRecords(domain);
+
 const domainParts = domain.split(".");
 const domainRoot = domainParts.slice(-2).join(".");
 const domainName = domainParts.slice(0,-2).join(".") || "@";
 
-console.log(domains)
+const pub_ip_provider = "https://api.ipify.org?format=json"
 
+let pub_ip = await fetch(pub_ip_provider)
+if (!pub_ip.ok) {
+	throw new Error(`Failed to fetch pub Ip from ${pub_ip_provider}`)
+}
+pub_ip = await pub_ip.json()
+pub_ip = pub_ip["ip"]
+const record_ip = records.find((r) => r.type === 'A')["content"]
+if (pub_ip === record_ip) {
+	console.log(`don't have to update ip. ${record_ip} === ${pub_ip}`)
+	Deno.exit(0)
+}
+console.log(`updating record ip. record: ${record_ip} !== public: ${pub_ip}`)
+await dns.update(domain, records.find((r) => r.type === 'A'), {content: pub_ip})
+console.log(`updated A-record for ${domain} to ${pub_ip}`)
