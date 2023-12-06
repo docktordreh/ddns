5f25049c34a457a8ff636e84762f0b8ac41bc867
valentin lechner <valentin_lechner@dismail.de>
2023-12-06 19:48:34 +0100

    changed to ipv6 for dslite-guys like me

--- ddns.ts
+++ ddns.ts
@@ -10,7 +10,7 @@ const domainParts = domain.split(".");
 const domainRoot = domainParts.slice(-2).join(".");
 const domainName = domainParts.slice(0,-2).join(".") || "@";
 
-const pub_ip_provider = "https://api.ipify.org?format=json"
+const pub_ip_provider = "https://api64.ipify.org?format=json"
 
 let pub_ip = await fetch(pub_ip_provider)
 if (!pub_ip.ok) {
@@ -18,11 +18,11 @@ if (!pub_ip.ok) {
 }
 pub_ip = await pub_ip.json()
 pub_ip = pub_ip["ip"]
-const record_ip = records.find((r) => r.type === 'A')["content"]
+const record_ip = records.find((r) => r.type === 'AAAA')["content"]
 if (pub_ip === record_ip) {
 	console.log(`don't have to update ip. ${record_ip} === ${pub_ip}`)
 	Deno.exit(0)
 }
 console.log(`updating record ip. record: ${record_ip} !== public: ${pub_ip}`)
-await dns.update(domain, records.find((r) => r.type === 'A'), {content: pub_ip})
+await dns.update(domain, records.find((r) => r.type === 'AAAA'), {content: pub_ip})
 console.log(`updated A-record for ${domain} to ${pub_ip}`)
